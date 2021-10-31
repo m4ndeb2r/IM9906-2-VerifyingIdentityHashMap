911362eb56a8ac21e3ca53fcb6c9e52636d90e63
martindeboer <m4ndeb2r@gmail.com>
2021-10-31 12:26:35 +0100

    Added equals contract to stubbed Object class

--- jre/java/lang/Object.java
+++ jre/java/lang/Object.java
@@ -7,6 +7,15 @@ public class Object {
    /**
     * @generated
     */
+   /*@ public normal_behavior
+     @   requires true;
+     @   ensures \result == this.equals(param0);
+     @*/
+   public /*@ helper strictly_pure @*/ boolean equals(/*@ nullable */ java.lang.Object param0);
+
+   /**
+    * @generated
+    */
    /*@ public behavior
      @ requires true;
      @ ensures true;
